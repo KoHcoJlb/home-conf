f7d2403e8f9b0827939711c87b84d430aacdaf7f
Ihor Stuzhuk <fujitsuigor@gmail.com>
2025-10-23 14:16:41 +0300

    Support for local nixpkgs overlays

--- home/dot_config/nix/flake/flake.nix
+++ home/dot_config/nix/flake/flake.nix
@@ -8,16 +8,23 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, ... } @ inputs: flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-  in {
-    legacyPackages = pkgs;
+  outputs =
+    { nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = if builtins.pathExists ./pkgs-overlays.nix then import ./pkgs-overlays.nix else [ ];
+        };
+      in
+      {
+        legacyPackages = pkgs;
 
-    packages = {
-      homeEnv = import ./packages.nix { inherit pkgs inputs; };
-    };
-  });
+        packages = {
+          homeEnv = import ./packages.nix { inherit pkgs inputs; };
+        };
+      }
+    );
 }
